46391ef7a8f92afa839ee8db2eedba48b3d94cf1
Lauri Vasama <git@vasama.org>
2024-12-30 17:23:46 +0200

    Add vsm.function/0.1 package

---
+++ library/function/include/vsm/detail/function_view.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <vsm/function_ptr.hpp>
+#include <vsm/platform.h>
+#include <vsm/utility.hpp>
+
+namespace vsm::detail {
+
+union function_context
+{
+	struct {} unused;
+	void const* object;
+	function_ptr_t function;
+};
+
+
+struct get_function_context_cpo {};
+//TODO: Use the get_function_context_cpo to optimise function_view.
+
+
+template<bool N, typename T, typename R, typename... Ps>
+/*constexpr*/ R invoke_object(function_context const context, Ps... args) noexcept(N)
+{
+	return const_cast<T&>(*static_cast<T const*>(context.object))(vsm_forward(args)...);
+}
+
+template<bool N, typename F, typename R, typename... Ps>
+/*constexpr*/ R invoke_function(function_context const context, Ps... args) noexcept(N)
+{
+	return static_cast<F*>(context.function)(vsm_forward(args)...);
+}
+
+template<bool N, auto F, typename R, typename... Ps>
+/*constexpr*/ R invoke_nontype_unused(function_context, Ps... args) noexcept(N)
+{
+	return F(vsm_forward(args)...);
+}
+
+template<bool N, auto F, typename T, typename R, typename... Ps>
+/*constexpr*/ R invoke_nontype_object(function_context const context, Ps... args) noexcept(N)
+{
+	return F(const_cast<T&>(*static_cast<T const*>(context.object)), vsm_forward(args)...);
+}
+
+} // namespace vsm::detail
