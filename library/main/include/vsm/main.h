d353e406c2556bd768afeb107758ae2a53111b85
Lauri Vasama <git@vasama.org>
2024-12-30 17:28:35 +0200

    Add vsm-main/0.1 package

---
+++ library/main/include/vsm/main.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int vsm_main(int argc, char const* const* argv);
+int vsm_main_entrypoint(int argc, char const* const* argv);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
---
+++ library/main/source/vsm/impl/main.cpp
@@ -0,0 +1,6 @@
+#include <vsm/main.h>
+
+int main(int const argc, char const* const* const argv)
+{
+	return vsm_main_entrypoint(argc, argv);
+}
