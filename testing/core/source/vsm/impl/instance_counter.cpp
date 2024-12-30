a1bde694aadda90625640a2df5f9f5ab840002a8
Lauri Vasama <git@vasama.org>
2024-12-30 17:02:08 +0200

    Add vsm-testing-core/0.1 package

---
+++ testing/core/source/vsm/impl/instance_counter.cpp
@@ -0,0 +1,7 @@
+#include <vsm/testing/instance_counter.hpp>
+
+using namespace vsm::test;
+using namespace vsm::test::detail;
+
+template instance_count_type detail::g_root_instance_count<instance_count_default_t>;
+template instance_count_type* detail::g_current_instance_count<instance_count_default_t>;
