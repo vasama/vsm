a44f625d2fa0dec207b0c2f4faa9b1155e914740
Lauri Vasama <git@vasama.org>
2025-06-18 15:49:11 +0300

    amend vsm.core: add memalignment

--- library/core/include/vsm/memory.hpp
+++ library/core/include/vsm/memory.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 
 namespace vsm {
+
+[[nodiscard]] inline size_t memalignment(void* const ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr) & ~reinterpret_cast<uintptr_t>(ptr) + 1;
+}
 
 void memswap(void* lhs, void* rhs, size_t size);
 
